8aee49dd94c5633e1ec616f064abb91951e0f7e4
sayali-2001 <sayalithakare47@gmail.com>
2022-09-09 11:14:22 +0530

    The sum of Command Line Arguments

---
+++ CommandLine.java
@@ -0,0 +1,10 @@
+public class CommandLine {
+    public static void main(String args[]) {
+        int sum=0;
+        int i;
+        for( i=0; i<args.length; i++){
+            sum = sum + Integer.parseInt(args[i]);
+        }
+        System.out.println("Sum of Numbers: " +sum);
+    }
+}
